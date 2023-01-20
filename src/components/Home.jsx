d0df90b617afd3a9003744f64189b7234bfe3d77
Tamunopreye <rrich3028@gmail.com>
2023-01-20 17:19:50 +0100

    Added mobile nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,51 +1,58 @@
-import React from "react"
+import { useState } from "react"
 import imageWeb3 from "../assets/images/image-web-3-desktop.jpg"
+import imageWeb3Phn from "../assets/images/image-web-3-mobile.jpg"
 import retroPics from "../assets/images/image-retro-pcs.jpg"
 import gamingGrowth from "../assets/images/image-gaming-growth.jpg"
 import topLapops from "../assets/images/image-top-laptops.jpg"
 
 const Home = () => {
+  const [change, setChange] = useState(true)
   return (
     <div>
-      <main className="grid md:grid-cols-3 gap-4 m-3 font-inter">
+      <main className="grid md:grid-cols-3 gap-4 m-6 font-inter">
         <section className="md:col-span-2">
           <img src={imageWeb3} alt="" width={1000} height="auto" />
           <div className="grid md:grid-cols-2 mt-4">
-            <h2 className="text-6xl font-bold max-w-xs">
+            <h2 className="max-w-sm my-3 text-5xl font-bold md:text-6xl mt-6 md:max-w-xs">
               The Bright Future of Web 3.0?
             </h2>
-            <div>
-              <p className="text-xl">
+            <div className="mt-6">
+              <p className="text-xl text-darkGrayishBlue">
                 We dive into the next evolution of the web that claims to put
                 the power of the platforms back into the hands of the people.
                 But is it really fulfillin its promise?
               </p>
-              <button className=" bg-softRed text-white text-center uppercase py-2 px-6 my-4">
+              <button className=" bg-softRed hover:bg-veryDarkBlue text-white font-bold text-center uppercase py-3 px-10 mt-6">
                 Read more
               </button>
             </div>
           </div>
         </section>
-
         {/* Second Section */}
-        <section className="bg-veryDarkBlue text-white p-4 text-xl ">
+        <section className="bg-veryDarkBlue text-white p-4 sm:text-md md:text-xl">
+          <h2 className="text-softOrange text-4xl font-bold mt-3 mb-6">New</h2>
           <div className="flex flex-col space-y-9">
-            <h2>New</h2>
-            <div>
-              <h3>Hydrogen VS Electric Cars</h3>
+            <div className="space-y-1">
+              <h3 className="font-bold text-2xl hover:text-softOrange cursor-pointer">
+                Hydrogen VS Electric Cars
+              </h3>
               <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
             </div>
             <hr />
-            <div>
-              <h3>The Downsides of AI Artistry</h3>
+            <div className="space-y-1">
+              <h3 className="font-bold text-2xl hover:text-softOrange cursor-pointer">
+                The Downsides of AI Artistry
+              </h3>
               <p>
                 What are the possible adverse effects of on-demand AI image
                 generation?
               </p>
             </div>
             <hr />
-            <div>
-              <h3>Is VC Funding Drying Up?</h3>
+            <div className="space-y-1">
+              <h3 className="font-bold text-2xl hover:text-softOrange cursor-pointer">
+                Is VC Funding Drying Up?
+              </h3>
               <p>
                 Private funding by VC firms is down 50% YOY. We take a look at
                 what that means.
@@ -54,33 +61,47 @@ const Home = () => {
           </div>
         </section>
 
-        {/* Third Section
-        <section className="col-span-3 flex justify-between">
-          <div className="flex">
-            <img src={retroPics} alt="" />
-            <div>
-              <p>01</p>
-              <p>Reviving Retro PCs</p>
-              <p>What happens when old PCs are given modern upgrades?</p>
+        {/* Third Section */}
+        <section className="md:col-span-3 mt-6">
+          <div className="flex flex-col gap-2 md:flex-row">
+            <div className="flex gap-4">
+              <img src={retroPics} width={120} height={"auto"} alt="" />
+              <div className="">
+                <h3 className="text-grayishBlue text-4xl font-bold ">01</h3>
+                <p className="text-2xl font-bold mt-3 md:text-xl cursor-pointer hover:text-softRed">
+                  Reviving Retro PCs
+                </p>
+                <p className="text-darkGrayishBlue mt-4">
+                  What happens when old PCs are given modern upgrades?
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <img src={topLapops} width={120} height="auto" alt="" />
+              <div className="">
+                <h3 className="text-grayishBlue text-4xl font-bold ">02</h3>
+                <p className="text-2xl font-bold mt-3 md:text-xl cursor-pointer hover:text-softRed">
+                  Top 10 Laptops of 2022
+                </p>
+                <p className="text-darkGrayishBlue mt-4">
+                  Our best picks for various needs and budgets.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <img src={gamingGrowth} width={120} height="auto" alt="" />
+              <div className="">
+                <h3 className="text-grayishBlue text-4xl font-bold ">03</h3>
+                <p className="text-2xl font-bold mt-3 md:text-xl cursor-pointer hover:text-softRed">
+                  The Growth of Gaming
+                </p>
+                <p className="text-darkGrayishBlue mt-4">
+                  How the pandemic has spread more opportunities.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="flex">
-            <img src={topLapops} alt="" />
-            <div>
-              <p>02</p>
-              <p>Top 10 Laptops of 2022</p>
-              <p>Our best picks for various needs and budgets.</p>
-            </div>
-          </div>
-          <div className="flex">
-            <img src={gamingGrowth} alt="" />
-            <div>
-              <p>03</p>
-              <p>The Growth of Gaming</p>
-              <p>How the pandemic has spread more opportunities.</p>
-            </div>
-          </div>
-        </section> */}
+        </section>
       </main>
     </div>
   )
